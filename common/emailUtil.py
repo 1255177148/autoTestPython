75a80a42721ccc24e905251e5fd012206cd7ed5f
elvis <1255177148@qq.com>
2023-08-01 16:59:27 +0800

    发送邮件(一)

--- common/emailUtil.py
+++ common/emailUtil.py
@@ -1,1 +1,8 @@
-from conf import config
+from Conf import config
+import smtplib
+import os
+import logging
+from email import encoders
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
